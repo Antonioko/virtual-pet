ac2e77ff697d12b5622dc3e09dbf93d3249c9fe8
anton_kpv <50264835+Antonioko@users.noreply.github.com>
2021-06-27 12:27:47 +0200

    Update animal.java

--- com/company/animal.java
+++ com/company/animal.java
@@ -4,10 +4,10 @@ public class animal {
     private int happyness;
     private int hunger;
     public String color;
-        public animal(String myColor){
+        public animal(String myColor, String animalType){
             happyness=0;
             hunger = 5;
             color = myColor;
-            System.out.println("new cat created");
+            System.out.println("new "+animalType " created");
     }
 }
